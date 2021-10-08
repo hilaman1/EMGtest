374a7f80ec61ff6eaf796326d9381c05e5f09759
hilaman1@mail.tau.ac.il <Leaveme2020>
2021-10-08 22:34:38 +0300

    version1

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
+import mne
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# A Notch Filter is a bandstop filter with a very narrow stopband and two passbands,
+# it actually highly attenuates/eliminates a particular frequency component from the input
+# signal while leaving the amplitude of the other frequencies more or less unchanged.
 
+# Create/view notch filter
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+samp_freq = 100  # Sample frequency (Hz)
+notch_freq = 50.0  # Frequency to be removed from signal (Hz)
+quality_factor = 20.0  # Quality factor
 
+# Design a notch filter using signal.iirnotch
+b_notch, a_notch = signal.iirnotch(notch_freq, quality_factor, samp_freq)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Compute magnitude response of the designed filter
+freq, h = signal.freqz(b_notch, a_notch, fs=samp_freq)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fig = plt.figure(figsize=(8, 6))
+
+# Plot magnitude response of the filter
+plt.plot(freq * samp_freq / (2 * np.pi), 20 * np.log10(abs(h)),
+         'r', label='Bandpass filter', linewidth='2')
+plt.xlabel('Frequency [Hz]', fontsize=20)
+plt.ylabel('Magnitude [dB]', fontsize=20)
+plt.title('Notch Filter', fontsize=20)
+plt.grid()
+file_path = '001_0.edf'
+noisySignal = mne.io.read_raw_edf(file_path, preload=True)
+
+# Plotting
+fig = plt.figure(figsize=(8, 6))
+plt.subplot(211)
+plt.plot(n, noisySignal, color='r', linewidth=2)
+plt.xlabel('Time', fontsize=20)
+plt.ylabel('Magnitude', fontsize=18)
+plt.title('Noisy Signal', fontsize=20)
+
+# Apply notch filter to the noisy signal using signal.filtfilt
+outputSignal = signal.filtfilt(b_notch, a_notch, noisySignal)
+
+# Plot notch-filtered version of signal
+plt.subplot(212)
+
+# Plot output signal of notch filter
+plt.plot(n, outputSignal)
+plt.xlabel('Time', fontsize=20)
+plt.ylabel('Magnitude', fontsize=18)
+plt.title('Filtered Signal', fontsize=20)
+plt.subplots_adjust(hspace=0.5)
+fig.tight_layout()
+plt.show()
